f7256a9dd4209a40337481268eed3424c559bc85
7roden <109483109+7roden@users.noreply.github.com>
2022-11-22 20:16:53 +0200

    22112022 (everything is possible)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-$(function(){
-
+/*
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true
 });
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
+*/
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  })
